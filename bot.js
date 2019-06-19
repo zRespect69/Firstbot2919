8fffca2310dbe3e9648e8d0d602b5efcb73a65e8
zRespect69 <51334815+zRespect69@users.noreply.github.com>
2019-06-19 12:46:49 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,7 +1,6 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
-const devs = ["535979538551930891"];
-const adminprefix = ["res"];
+
 
 client.on('ready',  () => {
   console.log('~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~'); 
@@ -14,28 +13,43 @@ client.user.setStatus("dnd");
 });
 
 
+
+
+client.login(process.env.BOT_TOKEN);
+
+
+const devs = ["535979538551930891"];
+const adminprefix = ["!"];
 client.on('message', message => {
-  var argresult = message.content.split(`).slice(1).join(' ');
-    if (!devs.includes(message.author.id)) return;
-    
-if (message.content.startsWith('!setgame')) {
-  client.user.setGame(argresult);
-} else 
-  if (message.content.startsWith('!setname')) {
-client.user.setUsername(argresult).then
-    message.channel.sendMessage(${argresult} : تم تغيير أسم البوت إلى`)
-} else
-  if (message.content.startsWith('!setavatar')) {
-client.user.setAvatar(argresult);
-      } else
-  if (message.content.startsWith('!wt')) {
+    var argresult = message.content.split(` `).slice(1).join(' ');
+      if (!devs.includes(message.author.id)) return;
+      
+  if (message.content.startsWith('arply')) {
+    client.user.setGame(argresult);
+      message.channel.send(`**✅   ${argresult}**`)
+  } else 
+     if (message.content === ("leave")) {
+    message.guild.leave();        
+  } else  
+  if (message.content.startsWith('arwt')) {
   client.user.setActivity(argresult, {type:'WATCHING'});
-  } else
-  if (message.content.startsWith('!ls')) {
+      message.channel.send(`**✅   ${argresult}**`)
+  } else 
+  if (message.content.startsWith('arls')) {
   client.user.setActivity(argresult , {type:'LISTENING'});
-  } else
-if (message.content.startsWith('!sett')) {
-  client.user.setGame(argresult, "https://www.twitch.tv/xkilleryt%22);
+      message.channel.send(`**✅   ${argresult}**`)
+  } else 
+  if (message.content.startsWith('arst')) {
+    client.user.setGame(argresult, "https://www.twitch.tv/idk");
+      message.channel.send(`**✅**`)
+  }
+  if (message.content.startsWith('arsetname')) {
+  client.user.setUsername(argresult).then
+      message.channel.send(`Changing The Name To ..**${argresult}** `)
+} else
+if (message.content.startsWith('arsetavatar')) {
+  client.user.setAvatar(argresult);
+    message.channel.send(`Changing The Avatar To :**${argresult}** `);
 }
 });
 
@@ -43,4 +57,12 @@ if (message.content.startsWith('!sett')) {
 
 
 
-client.login(process.env.BOT_TOKEN);
+Spam1.on('ready', async() => {
+    console.log('hello')
+    const server = "590851546875559946"; // ايدي السررفر
+const channel = "590851546875559948";//ايدي الروم
+    setInterval(()=>{
+    Spam1.guilds.get(server).channels.get(channel).send(`**Hiiiii :)**`)//spamming message.
+},1000);//Delay Time
+})
+ 
