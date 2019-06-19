21ea2b73269ab265bb05ef8dc4593c905e612b3a
zRespect69 <51334815+zRespect69@users.noreply.github.com>
2019-06-19 11:32:12 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,14 +1,6 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
 
-client.on('message', message => {
-  if (message.author.bot) return;
-  if (message.content === prefix + "help") {
-      message.channel.send(`**:information_source: | Help Menu in DM , رسالة المساعدة في الخاص .**`);
-
-
-  }
-});
 
 
 client.login(process.env.BOT_TOKEN);
