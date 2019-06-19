1523e2a877a682ed00b24954a0d40556fec98dd5
zRespect69 <51334815+zRespect69@users.noreply.github.com>
2019-06-19 13:15:45 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -57,12 +57,55 @@ if (message.content.startsWith('arsetavatar')) {
 
 
 
-client.on('ready', async() => {
-    console.log('hello')
-    const server = "590851546875559946"; // ايدي السررفر
-const channel = "590851546875559948";//ايدي الروم
-    setInterval(()=>{
-    Spam1.guilds.get(server).channels.get(channel).send(`**Hiiiii :)**`)//spamming message.
-},1000);//Delay Time
-})
- 
+const Discord = require("discord.js");
+const client = new Discord.Client();
+const bot = new Discord.Client();
+const hero = client;
+const Data = {};
+const fs = require("fs");
+const ms = require("ms");
+client.on('ready', () => {
+    console.log(`Logged in as ${client.user.tag}!`);
+});
+
+
+
+client.on('message', msg => {
+  if (msg.content === ("!settings")) {
+      const embed = new Discord.RichEmbed()
+          .setAuthor('❯ Settings:', 'https://labs-public-dl.xda-cdn.com/images/af6aef8c-4d0a-41f4-8afb-5b8572aa3697.png')
+          .addField(`❯ Values: `, `» \`limitsban\`, \`limitskick\`, \`limitsroleD\`, \`limitsroleC\`, \`limitschannelD\`, \`limitstime\`, \`setMedia\`, \`infoMedia\`, \`toggleMedia\`, \`setwel\`, \`setrole\`, \`AntiFake\`, \`SetFake\`, \`SetSug\`, \`AntiBots\``)
+          .addField(`❯ Commands: `, `» $settings [limitsban/limitschannelD/...] [vlaue]\n» $settings [AntiFake/AntiFake/...] [On/Off]`)
+      msg.channel.send(embed)
+
+  };
+});
+
+client.on('message', message => {
+  if (message.author.bot) return;
+  let wewa = hero.guilds.get("525365808352985088").emojis.find(r => r.name === "WEWA");
+  let RainBow = hero.guilds.get("525365808352985088").emojis.find(r => r.name === "RainBowS");
+  let Band = hero.guilds.get("525365808352985088").emojis.find(r => r.name === "BanS");
+  let gl = hero.guilds.get("525365808352985088").emojis.find(r => r.name === "GlowPartner");
+  let ss = hero.guilds.get("525365808352985088").emojis.find(r => r.name === "NONOFUCKYOU");
+  let xx = hero.guilds.get("525365808352985088").emojis.find(r => r.name === "NOO");
+  let zg = client.guilds.get("525365808352985088").emojis.find(r => r.name === "Money");
+  let qq = client.guilds.get("537028830943248385").emojis.find(r => r.name === "524948763890941952");
+  let t = hero.guilds.get("522904523455594496").emojis.find(r => r.name === "true");
+  let f = hero.guilds.get("522904523455594496").emojis.find(r => r.name === "false");
+  if (message.content.startsWith("!help")) {
+      message.author.send(`**
+❯ Informetion Commands | الأوامر التعريفية :
+» \`$stats\` : معلومات عن البوت
+» \`$settings\` : رؤية ما يمكن إعدادة في البوت
+» \`$ping\` : رؤية سرعة البوت
+» \`$invite\` : لدعوة البوت**
+`)
+
+
+  }
+});
+
+client.on('message', message => {
+  if (message.author.bot) return;
+  let Band = hero.guilds.get("525365808352985088").emojis.find(r => r
