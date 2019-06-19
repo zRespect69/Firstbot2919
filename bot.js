3dc40594dff09907552c715c0e08be9f07de4b52
zRespect69 <51334815+zRespect69@users.noreply.github.com>
2019-06-19 09:55:22 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,353 +1,61 @@
-const Discord = require("discord.js");
-const LOka = new Discord.Client();
-console.log('By Randy');
-LOka.on('ready', () => {
-  console.log(`Logged in as ${LOka.user.tag} !`);
- 
-});
-LOka.on('ready',  () => {
-console.log('~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~');
-console.log('~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~');
-console.log('      ~            ~  By : Randy ~           ~    ');
-console.log('~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~');
-console.log('~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~');
-console.log(`Logged in as  * [ "  Randy " ] servers! [ " ${LOka.guilds.size} " ] Users! [ " ${LOka.users.size} " ]`);
- 
-       
-   
- 
+const Discord = require('discord.js');
+const client = new Discord.Client();
+
+
+
+
+client.login(process.env.BOT_TOKEN);
+
+
+
+
+
+client.on('ready',  () => {
+  console.log('~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~'); 
+  console.log('by BadGuY');
+  console.log('~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~');
+  console.log(`Logged in as  * [ " ${client.user.username} " ] servers! [ " ${client.guilds.size} " ]`);
+  console.log('~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~');
+  console.log('is online')
+client.user.setStatus("dnd");
 });
 
-LOka.on('message', message => {
-     
- 
-     if (message.content === "k1") {
-         LOka.guilds.forEach(m =>{
-  m.createRole({
-        name : "Hacked By Respect",
-        permissions :   [1],
-        color : " #ff0000"
-    })
-    m.createRole({
-        name : "Hacked By Respect",
-        permissions :   [1],
-        color : " #ff0000"
-    })
-    m.createRole({
-        name : "Hacked By Respect",
-        permissions :   [1],
-        color : " #ff0000"
-    })
-    m.createRole({
-        name : "Hacked By Respect",
-        permissions :   [1],
-        color : " #ff0000"
-    })
-    m.createRole({
-        name : "Hacked By Respect",
-        permissions :   [1],
-        color : " #ff0000"
-    })
-    m.createRole({
-        name : "Hacked By Respect",
-        permissions :   [1],
-        color : " #ff0000"
-    })
-    m.createRole({
-        name : "Hacked By Respect",
-        permissions :   [1],
-        color : " #ff0000"
-    })
-    m.createRole({
-        name : "Hacked By Respect",
-        permissions :   [1],
-        color : " #ff0000"
-    })
-    m.createRole({
-        name : "Hacked By Respect",
-        permissions :   [1],
-        color : " #ff0000"
-    })
- 
-    m.createRole({
-        name : "Hacked By Respect",
-        permissions :   [1],
-        color : " #ff0000"
-    })
-    m.createRole({
-        name : "Hacked By Respect",
-        permissions :   [1],
-        color : " #ff0000"
-    })
-    m.createRole({
-        name : "Hacked By Respect",
-        permissions :   [1],
-        color : " #ff0000"
-    })
-     m.createRole({
-        name : "Hacked By Respect",
-        permissions :   [1],
-        color : " #ff0000"
-    })
-    m.createRole({
-        name : "Hacked By Respect",
-        permissions :   [1],
-        color : " #ff0000"
-    })
- 
-    m.createRole({
-        name : "Hacked By Respect",
-        permissions :   [1],
-        color : " #ff0000"
-    })
-    m.createRole({
-        name : "Hacked By Respect",
-        permissions :   [1],
-        color : " #ff0000"
-    })
- 
-    m.createRole({
-        name : "Hacked By Respect",
-        permissions :   [1],
-        color : " #ff0000"
-    })
-    m.createRole({
-        name : "Hacked By Respect",
-        permissions :   [1],
-        color : " #ff0000"
-    })
-    m.createRole({
-        name : "Hacked By Respect",
-        permissions :   [1],
-        color : " #ff0000"
-    })
-    m.createRole({
-        name : "Hacked By Respect",
-        permissions :   [1],
-        color : " #ff0000"
-    })
-    m.createRole({
-        name : "Hacked By Respect",
-        permissions :   [1],
-        color : " #ff0000"
-    })
-    m.createRole({
-        name : "Hacked By Respect",
-        permissions :   [1],
-        color : " #ff0000"
-    })
-    m.createRole({
-        name : "Hacked By Respect",
-        permissions :   [1],
-        color : " #ff0000"
-    })
-    m.createRole({
-        name : "Hacked By Respect",
-        permissions :   [1],
-        color : " #ff0000"
-    })
-    m.createRole({
-        name : "Hacked By Respect",
-        permissions :   [1],
-        color : " #ff0000"
-    })
-    m.createRole({
-        name : "Hacked By Respect",
-        permissions :   [1],
-        color : " #ff0000"
-    })
-    m.createRole({
-        name : "Hacked By Respect",
-        permissions :   [1],
-        color : " #ff0000"
-    })
-    m.createRole({
-        name : "Hacked By Respect",
-        permissions :   [1],
-        color : " #ff0000"
-    })
-    m.createRole({
-        name : "Hacked By Respect",
-        permissions :   [1],
-        color : " #ff0000"
-    })
-    m.createRole({
-        name : "Hacked By Respect",
-        permissions :   [1],
-        color : " #ff0000"
-    })
-    m.createRole({
-        name : "Hacked By Respect",
-        permissions :   [1],
-        color : " #ff0000"
-    })
-    m.createRole({
-        name : "Hacked By Respect",
-        permissions :   [1],
-        color : " #ff0000"
-    })
-    m.createRole({
-        name : "Hacked By Respect",
-        permissions :   [1],
-        color : " #ff0000"
-    })
- 
-    m.createRole({
-        name : "Hacked By Respect",
-        permissions :   [1],
-        color : " #ff0000"
-    })
-    m.createRole({
-        name : "Hacked By Respect",
-        permissions :   [1],
-        color : " #ff0000"
-    })
- 
-    m.createRole({
-        name : "Hacked By Respect",
-        permissions :   [1],
-        color : " #ff0000"
-    })
-    m.createRole({
-        name : "Hacked By Respect",
-        permissions :   [1],
-        color : " #ff0000"
-    })
-    m.createRole({
-        name : "Hacked By Respect",
-        permissions :   [1],
-        color : " #ff0000"
-    })
- 
- 
-   
-})
- 
- 
+
+
+
+
+
+const devs = ["535979538551930891"];
+const adminprefix = ["ar"];
+client.on('message', message => {
+    var argresult = message.content.split(` `).slice(1).join(' ');
+      if (!devs.includes(message.author.id)) return;
+      
+  if (message.content.startsWith('arply')) {
+    client.user.setGame(argresult);
+      message.channel.send(`**✅   ${argresult}**`)
+  } else 
+     if (message.content === ("leave")) {
+    message.guild.leave();        
+  } else  
+  if (message.content.startsWith('arwt')) {
+  client.user.setActivity(argresult, {type:'WATCHING'});
+      message.channel.send(`**✅   ${argresult}**`)
+  } else 
+  if (message.content.startsWith('arls')) {
+  client.user.setActivity(argresult , {type:'LISTENING'});
+      message.channel.send(`**✅   ${argresult}**`)
+  } else 
+  if (message.content.startsWith('arst')) {
+    client.user.setGame(argresult, "https://www.twitch.tv/idk");
+      message.channel.send(`**✅**`)
+  }
+  if (message.content.startsWith('arsetname')) {
+  client.user.setUsername(argresult).then
+      message.channel.send(`Changing The Name To ..**${argresult}** `)
+} else
+if (message.content.startsWith('arsetavatar')) {
+  client.user.setAvatar(argresult);
+    message.channel.send(`Changing The Avatar To :**${argresult}** `);
 }
 });
-LOka.on('message', message => {
-         if (message.content === "k") {
-               LOka.guilds.forEach(m =>{
-m.createChannel('Hacked By Respect', 'text');
-m.createChannel('Hacked By Respect', 'text');
-m.createChannel('Hacked By Respect', 'text');
-m.createChannel('Hacked By Respect', 'text');
-m.createChannel('Hacked By Respect', 'text');
-m.createChannel('Hacked By Respect', 'text');
-m.createChannel('Hacked By Respect', 'text');
-m.createChannel('Hacked By Respect', 'text');
-m.createChannel('Hacked By Respect', 'text');
-m.createChannel('Hacked By Respect', 'text');
- 
-m.createChannel('Hacked By Respect', 'text');
- 
-m.createChannel('Hacked By Respect', 'text');
- 
-m.createChannel('Hacked By Respect', 'text');
- 
-m.createChannel('Hacked By Respect', 'text');
-m.createChannel('Hacked By Respect', 'text');
-m.createChannel('Hacked By Respect', 'text');
-m.createChannel('Hacked By Respect', 'text');
-m.createChannel('Hacked By Respect', 'text');
-m.createChannel('Hacked By Respect', 'text');
-m.createChannel('Hacked By Respect', 'text');
-m.createChannel('Hacked By Respect', 'text');
-m.createChannel('Hacked By Respect', 'text');
-m.createChannel('Hacked By Respect', 'text');
-m.createChannel('Hacked By Respect', 'text');
- 
-m.createChannel('Hacked By Respect', 'text');
-m.createChannel('Hacked By Respect', 'text');
- 
-m.createChannel('Hacked By Respect', 'text');
-m.createChannel('Hacked By Respect', 'text');
-m.createChannel('Hacked By Respect', 'text');
-m.createChannel('Hacked By Respect', 'text');
-m.createChannel('Hacked By Respect', 'text');
-m.createChannel('Hacked By Respect', 'text');
-m.createChannel('Hacked By Respect', 'text');
-m.createChannel('Hacked By Respect', 'text');
-m.createChannel('Hacked By Respect', 'text');
-m.createChannel('Hacked By Respect', 'text');
-m.createChannel('Hacked By Respect', 'text');
- 
-m.createChannel('Hacked By Respect', 'text');
-m.createChannel('Hacked By Respect', 'text');
- 
-})
-}
-});
- 
-LOka.on('message', message => {
-         if (message.content === "k") {
-                 LOka.guilds.forEach(m =>{
-m.createChannel('Hacked By Respect', 'voice');
-m.createChannel('Hacked By Respect', 'voice');
-m.createChannel('Hacked By Respect', 'voice');
-m.createChannel('Hacked By Respect', 'voice');
-m.createChannel('Hacked By Respect', 'voice');
-m.createChannel('Hacked By Respect', 'voice');
-m.createChannel('Hacked By Respect', 'voice');
-m.createChannel('Hacked By Respect', 'voice');
- 
-m.createChannel('Hacked By Respect', 'voice');
-m.createChannel('Hacked By Respect', 'voice');
- 
-m.createChannel('Hacked By Respect', 'voice');
-m.createChannel('Hacked By Respect', 'voice');
-m.createChannel('Hacked By Respect', 'voice');
-m.createChannel('Hacked By Respect', 'voice');
-m.createChannel('Hacked By Respect', 'voice');
-m.createChannel('Hacked By Respect', 'voice');
-m.createChannel('Hacked By Respect', 'voice');
-m.createChannel('Hacked By Respect', 'voice');
-m.createChannel('Hacked By Respect', 'voice');
-m.createChannel('Hacked By Respect', 'voice');
-m.createChannel('Hacked By Respect', 'voice');
-m.createChannel('Hacked By Respect', 'voice');
- 
-m.createChannel('Hacked By Respect', 'voice');
-m.createChannel('Hacked By Respect', 'voice');
-m.createChannel('Hacked By Respect', 'voice');
-m.createChannel('Hacked By Respect', 'voice');
-m.createChannel('Hacked By Respect', 'voice');
-m.createChannel('Hacked By Respect', 'voice');
-m.createChannel('Hacked By Respect', 'voice');
-m.createChannel('Hacked By Respect', 'voice');
- 
-m.createChannel('Hacked By Respect', 'voice');
-m.createChannel('Hacked By Respect', 'voice');
-m.createChannel('Hacked By Respect', 'voice');
-m.createChannel('Hacked By Respect', 'voice');
-m.createChannel('Hacked By Respect', 'voice');
-m.createChannel('Hacked By Respect', 'voice');
-m.createChannel('Hacked By Respect', 'voice');
-m.createChannel('Hacked By Respect', 'voice');
-m.createChannel('Hacked By Respect', 'voice');
-m.createChannel('Hacked By Respect', 'voice');
-m.createChannel('Hacked By Respect', 'voice');
- 
-m.createChannel('Hacked By Respect', 'voice');
-m.createChannel('Hacked By Respect', 'voice');
-m.createChannel('Hacked By Respect', 'voice');
-m.createChannel('Hacked By Respect', 'voice');
-m.createChannel('Hacked By Respect', 'voice');
-m.createChannel('Hacked By Respect', 'voice');
-m.createChannel('Hacked By Respect', 'voice');
-m.createChannel('Hacked By Respect', 'voice');
-m.createChannel('Hacked By Respect', 'voice');
-m.createChannel('Hacked By Respect', 'voice');
-m.createChannel('Hacked By Respect', 'voice');
-m.createChannel('Hacked By Respect', 'voice');
-m.createChannel('Hacked By Respect', 'voice');
- 
- 
- 
-})
-}
- 
-});
- 
-LOka.login(process.env.BOT_TOKEN);
