d6abaa4221d4635c8b15c67e79d13bd5260ebb63
zRespect69 <51334815+zRespect69@users.noreply.github.com>
2019-06-22 20:01:57 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -24,28 +24,6 @@ if(!message.channel.guild) return;
   }
 });
 
-client.on("message", message => {
-    var prefix = "#";
-            var args = message.content.substring("#clear").split(" ");
-            if (message.content.startsWith("#clear")) {
- if (!args[1]) {
-                                let x5bz1 = new Discord.RichEmbed()
-                                .setDescription("#clear <number>")
-                                .setColor("#0000FF")
-                                message.channel.sendEmbed(x5bz1);
-                            } else {
-                            let messagecount = parseInt(args[1]);
-                            message.channel.fetchMessages({limit: messagecount}).then(messages => message.channel.bulkDelete(messages));
-                                                          message.channel.fetchMessages({limit: messagecount}).then(messages => message.channel.bulkDelete(messages));
-                            message.channel.fetchMessages({limit: messagecount}).then(messages => message.channel.bulkDelete(messages));
-                            let x5bz2 = new Discord.RichEmbed()
-                                                            .setColor("#008000")
-                                .setDescription(":white_check_mark: | Delete " + args[1] + " Message!")
-                                                                                        message.delete("..");
-                                message.channel.sendEmbed(x5bz2);
-                            }
-                          }
-});
 
 
 
