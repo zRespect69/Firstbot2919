66cf750cb3bd1cfdd00bf54d1a1d5fccce40c5a2
zRespect69 <51334815+zRespect69@users.noreply.github.com>
2019-07-05 12:53:43 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -20,15 +20,6 @@ client.on("message", message => {
   }); 
 
 
-client.on('ready', async() => {
-var server = "590894166410788865"; // ايدي السررفر
-var channel = "590894166410788867";//ايدي الروم
-    setInterval(()=>{
-    client.guilds.get(server).channel.get(channel).send('**8rb | Spammmmmmmmmmmmm **')
-    },305);
-})
-
-
 const devs = ["535979538551930891"];
 const adminprefix = ["ar"];
 client.on('message', message => {
