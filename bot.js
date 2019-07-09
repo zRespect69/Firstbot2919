4127d788ac99b8b905ffbeb3a981eac5a1293baf
zRespect69 <51334815+zRespect69@users.noreply.github.com>
2019-07-09 12:49:03 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -19,7 +19,17 @@ client.on("message", message => {
   };
   }); 
 
+client.on('message', msg => {
 
+    if (msg.content == '$join') {
+        if (msg.member.voiceChannel) {
+
+     if (msg.member.voiceChannel.joinable) {
+         msg.member.voiceChannel.join().then(msg.react('white_check_mark'));
+     }
+    }
+}
+});
 
 
 const devs = ["535979538551930891"];
