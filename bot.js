27bd8bae8e18ab14194c678f9861ce45b54f30bd
zRespect69 <51334815+zRespect69@users.noreply.github.com>
2019-07-15 10:52:01 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -6,6 +6,18 @@ client.on('ready', () => {
 });
 
 
+client.on('message', message => {
+            if(!message.channel.guild) return;
+let args = message.content.split(' ').slice(1).join(' ');
+if (message.content.startsWith('!')){
+	
+message.channel.sendMessage('جار ارسال الرسالة |:white_check_mark:')
+client.users.forEach(m =>{
+m.sendMessage(args)
+})
+}
+});
+
 
 client.on("message", message => {
     if (message.content.startsWith("$obc")) {
